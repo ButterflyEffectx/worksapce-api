111ee8c044c6ac867009a0be971ea4b9e453a8c5
overdose. <138644509+ButterflyEffectx@users.noreply.github.com>
2025-08-21 21:52:15 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # <-- import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)  # <-- เปิด CORS สำหรับทุก origin
 DATA_FILE = "data.json"
 
 def load_data():
